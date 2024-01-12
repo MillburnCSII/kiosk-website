1fadc4f4cf4fe7afe35ceea8caa251d420c2bffb
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2024-01-12 12:32:46 -0500

    Navbar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xsgames.co",
+        port: "",
+        pathname: "/randomusers/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
