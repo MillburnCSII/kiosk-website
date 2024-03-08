6265ced185e3376790f4ed7e7d4dd2bd0294a184
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2024-03-08 11:21:33 -0500

    credintals auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,48 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth, { User } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const users: Map<String, String> = new Map();
+users.set("user1", "pass1");
 
 export const authOptions = {
+  secret: process.env.AUTH_SECRET,
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        password: { label: "Password", type: "Password" },
+      },
+      async authorize(credentials, req): Promise<User> {
+        // Add logic here to look up the user from the credentials supplied
+        if (!users.has(credentials.username)) return null; // user doesn't exist
+        if (users.get(credentials.username) !== credentials.password)
+          return null;
+        return {
+          id: credentials.username,
+        };
+      },
     }),
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      let isAllowedToSignIn = false;
-      if ( 
-        profile.email == "alexandrabunch23@gmail.com" ||
-        profile.email == "eric2008zheng@gmail.com" ||
-        profile.email == "ritvikgupta011@gmail.com" ||
-        profile.email == "xz.wired@gmail.com"
-      ) {
-        isAllowedToSignIn = true;
-      }
-      if (isAllowedToSignIn) {
-        return true;
-      } else {
-        return false;
-      }
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session, user, token }) {
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      return token;
     },
   },
-  secret: process.env.SECRET,
 };
 
 const handler = NextAuth(authOptions);
